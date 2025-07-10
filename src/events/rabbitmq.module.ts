4eb515e3f503f6796a0e5bcca2ad5a165ba53711
igorgaetan <igorkwenja@gmail.com>
2025-07-10 21:58:48 +0100

    Modification des providers de rabbitmq module

--- src/events/rabbitmq.module.ts
+++ src/events/rabbitmq.module.ts
@@ -1,16 +1,20 @@
-// rabbitmq.module.ts
+// src/events/rabbitmq.module.ts
 import { Module } from '@nestjs/common';
-import { ClientsModule } from '@nestjs/microservices';
-import { rabbitMqClient } from '../config/rabbitmq.client.js';
+import { RabbitMQService } from './rabbitmq.publisher';
+import { rabbitMqClient } from '../config/rabbitmq.client'; // ← ton client RabbitMQ
+import { rabbitMQConfig } from '../config/rabbitmq.config';
 
 @Module({
-  imports: [],
   providers: [
+    RabbitMQService,
     {
-      provide: 'RABBITMQ_CLIENT',
-      useValue: rabbitMqClient,
+      provide: rabbitMQConfig.name,
+      useValue: rabbitMqClient
     },
   ],
-  exports: ['RABBITMQ_CLIENT'],
+  exports: [
+    RabbitMQService,
+    rabbitMQConfig.name,
+  ],
 })
 export class RabbitMQModule {}
